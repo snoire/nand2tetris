9b8b2963329500ee0c5f121c0ac2f174c0207ae0
Snoire <snoire@qq.com>
2021-11-25 15:53:25 +0800

    pro04

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,88 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+//prestate=1 // unknown
+//LOOP:
+//    if (KBD > 0)
+//      curstate=-1; // a key is pressed
+//    else
+//      curstate=0;  // no key is pressed
+//
+//    if (curstate == prestate)
+//        jmp END
+//
+//    // fill or clear the screen
+//    @curstate
+//    D=M
+//    @SCREEN
+//    M=D
+//    // loop ..
+//
+//END:
+//    prestate=curstate;
+//    jmp LOOP
+
+    @prestate
+    M=1
+
+(LOOP)
+    // curstate = 0;
+    @curstate
+    M=0
+    // curstate = -1 if KBD > 0
+    @KBD
+    D=M
+    @NOKEY
+    D;JEQ
+    @curstate
+    M=-1
+(NOKEY)
+
+    // goto END if (curstate == prestate)
+    @curstate
+    D=M
+    @prestate
+    D=D-M
+    @END
+    D;JEQ
+
+// fill or clear the screen (depends on the curstate)
+    // R0 = SCREEN
+    @SCREEN
+    D=A
+    @R0
+    M=D
+    // R1 = SCREEN + MAX - 1
+    @KBD
+    D=A-1
+    @R1
+    M=D
+
+(FILL)
+    // fill or clear the screen
+    @curstate
+    D=M
+    @R1
+    A=M // use R1 as a pointer
+    M=D
+
+    // R1--
+    @R1
+    M=M-1
+    // if (R1 >= R0) goto FILL
+    D=M
+    @R0
+    D=D-M
+    @FILL
+    D;JGE
+
+(END)
+    // prestate = curstate;
+    @curstate
+    D=M
+    @prestate
+    M=D
+
+    // jmp LOOP
+    @LOOP
+    0;JMP
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -9,4 +9,34 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+// for (R2 = 0; R1 > 0; R1--) {
+//      R2 += R0;   
+// }
+
+    // R2 = 0
+    @R2
+    M=0
+
+(LOOP)
+    // if (R1 == 0) goto END
+    @R1
+    D=M
+    @END
+    D;JEQ
+
+    // R2 += R0;
+    @R0
+    D=M
+    @R2
+    M=D+M
+
+    // R1--
+    @R1
+    M=M-1
+    // goto LOOP
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
