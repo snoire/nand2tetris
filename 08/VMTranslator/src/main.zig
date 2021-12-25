e81df09f065607878cdec8756a6b7c26c55d01de
Snoire <snoire@qq.com>
2021-12-25 20:24:06 +0800

    handle multi-file programs

--- 08/VMTranslator/src/main.zig
+++ 08/VMTranslator/src/main.zig
@@ -9,26 +9,35 @@ pub fn main() anyerror!void {
     defer std.process.argsFree(allocator, args);
 
     if (args.len < 2) {
-        print("Usage: {s} <file.vm>\n", .{args[0]});
+        print("Usage: {s} <dir/file.vm>\n", .{args[0]});
         return;
     }
 
-    // open vm file
+    const filename = args[1];
     const cwd = std.fs.cwd();
-    const vmfile = try cwd.openFile(args[1], .{ .read = true });
-    defer vmfile.close();
+    const file = try cwd.openFile(filename, .{ .read = true });
+    defer file.close();
+
+    const stat = try file.stat();
+    var asmfilename: []u8 = undefined;
+    var outputDir: std.fs.Dir = undefined;
+
+    if (stat.kind == .File) {
+        const noextension = filename[0 .. filename.len - 2]; // file name without extension "vm"
+        asmfilename = try std.mem.concat(allocator, u8, &[_][]const u8{ noextension, "asm" });
+        outputDir = try cwd.openDir(std.fs.path.dirname(filename).?, .{ .iterate = true });
+    } else if (stat.kind == .Directory) {
+        asmfilename = try std.mem.concat(allocator, u8, &[_][]const u8{ filename, "/", std.fs.path.basename(filename), ".asm" });
+        outputDir = try cwd.openDir(filename, .{ .iterate = true });
+    } else {
+        unreachable;
+    }
+    defer allocator.free(asmfilename);
+    defer outputDir.close();
 
     // truncate asm file for writing
-    const vmfilename = args[1];
-    const noextension = vmfilename[0 .. vmfilename.len - 2]; // file name without extension "vm"
-    const asmfilename = try std.mem.concat(allocator, u8, &[_][]const u8{ noextension, "asm" });
-    defer allocator.free(asmfilename);
-
     const asmfile = try cwd.createFile(asmfilename, .{ .truncate = true });
     defer asmfile.close();
-
-    // translate
-    const reader = vmfile.reader();
     const writer = asmfile.writer();
 
     const linebuf = try allocator.alloc(u8, 1024);
@@ -37,22 +46,32 @@ pub fn main() anyerror!void {
     try codewriter.init(allocator, writer);
     defer codewriter.deinit();
 
-    var basename = std.fs.path.basename(vmfilename);
-    try codewriter.setFileName(basename[0 .. basename.len - 3]); // file name without extension ".vm"
+    // iterate over the outputDir to find the .vm files in it
+    var iter = outputDir.iterate();
+    while (try iter.next()) |entry| {
+        if (entry.kind == .File and std.mem.endsWith(u8, entry.name, ".vm")) {
+            const vmfile = try outputDir.openFile(entry.name, .{ .read = true });
+            defer vmfile.close();
 
-    while (try parser.nextLine(reader, linebuf)) |line| {
-        if (try parser.parseCMD(line)) |cmd| {
-            try writer.print("// {s}\n", .{line});
+            const reader = vmfile.reader();
+            try codewriter.setFileName(entry.name[0 .. entry.name.len - 3]); // file name without extension ".vm"
 
-            switch (cmd.type) {
-                .C_PUSH, .C_POP => try codewriter.pushpop(cmd),
-                .C_ARITHMETIC => try codewriter.arithmetic(cmd),
-                .C_LABEL => try codewriter.label(cmd),
-                .C_GOTO => try codewriter.goto(cmd),
-                .C_IF => try codewriter.if_goto(cmd),
-                .C_CALL => try codewriter.call(cmd),
-                .C_FUNCTION => try codewriter.function(cmd),
-                .C_RETURN => try codewriter.@"return"(),
+            // translate
+            while (try parser.nextLine(reader, linebuf)) |line| {
+                if (try parser.parseCMD(line)) |cmd| {
+                    try writer.print("// {s}\n", .{line});
+
+                    switch (cmd.type) {
+                        .C_PUSH, .C_POP => try codewriter.pushpop(cmd),
+                        .C_ARITHMETIC => try codewriter.arithmetic(cmd),
+                        .C_LABEL => try codewriter.label(cmd),
+                        .C_GOTO => try codewriter.goto(cmd),
+                        .C_IF => try codewriter.if_goto(cmd),
+                        .C_CALL => try codewriter.call(cmd),
+                        .C_FUNCTION => try codewriter.function(cmd),
+                        .C_RETURN => try codewriter.@"return"(),
+                    }
+                }
             }
         }
     }
